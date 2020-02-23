b621c8e1a9f580fcca5dacb6d19bdd486f7a40de
Paul Blay <paulmblay@gmail.com>
2020-02-23 14:59:03 +0000

    minor renames to get includes working

--- features/step_definitions/BoostCalculatorSteps.cpp
+++ features/step_definitions/BoostCalculatorSteps.cpp
@@ -1,4 +1,4 @@
 #include <boost/test/unit_test.hpp>
 // Pretend to be GTest
 #define EXPECT_EQ BOOST_CHECK_EQUAL
-#include "stringSteps.cpp"
+#include "calculatorSteps.cpp"
--- features/step_definitions/stringSteps.cpp
+++
@@ -1,7 +0,0 @@
-#include <cucumber-cpp/autodetect.hpp>
-
-#include <string>
-
-using cucumber::ScenarioScope;
-
-GIVEN("^I have created ")
